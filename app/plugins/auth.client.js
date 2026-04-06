b57ac286c9e96b37c36b4227b98348df293db22a
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2026-04-06 19:52:23 -0300

    temporary disabled plugin for fetch user

--- app/plugins/auth.client.js
+++ app/plugins/auth.client.js
@@ -1,5 +1,9 @@
+/**
+ * TEMPORARIAMENTE DESABILITADO, ATIVAR QUANDO A API ESTIVER PRONTA
+ */
+
 /* Função para recarregar o auth.user quando a página é recarregada */
-export default defineNuxtPlugin(async () => {
-    const { fetchUser } = useAuth()
-    await fetchUser()
-})
+// export default defineNuxtPlugin(async () => {
+//     const { fetchUser } = useAuth()
+//     await fetchUser()
+// })
