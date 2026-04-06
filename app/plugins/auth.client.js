5be4c2b969ef6bb50e10231f9b3c89c000f18535
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2026-04-06 19:58:21 -0300

    Revert "temporary disabled plugin for fetch user"
    This reverts commit b57ac286c9e96b37c36b4227b98348df293db22a.

--- app/plugins/auth.client.js
+++ app/plugins/auth.client.js
@@ -1,9 +1,5 @@
-/**
- * TEMPORARIAMENTE DESABILITADO, ATIVAR QUANDO A API ESTIVER PRONTA
- */
-
 /* Função para recarregar o auth.user quando a página é recarregada */
-// export default defineNuxtPlugin(async () => {
-//     const { fetchUser } = useAuth()
-//     await fetchUser()
-// })
+export default defineNuxtPlugin(async () => {
+    const { fetchUser } = useAuth()
+    await fetchUser()
+})
